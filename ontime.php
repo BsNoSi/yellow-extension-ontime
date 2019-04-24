4cd5ab95d34aab5f7cd7c8662fc7346fa1dd02c2
BsNoSi <github@buoa.de>
2019-04-24 15:16:18 +0200

    Update for YELLOW 0.8.4 or higher

--- ontime.php
+++ ontime.php
@@ -1,13 +1,16 @@
 <?php
-// OnTime plugin, https://github.cmo/bsnosi/yellow-plugin-ontime
+// OnTime extension, https://github.cmo/bsnosi/yellow-extension-ontime
+// Copyright ©2018-now Norbert Simon, https://nosi.de for
+// YELLOW Copyright ©2013-now Datenstrom, http://datenstrom.se
 // idea based on http://github.com/schulle4u/yellow-plugin-global (version 0.7.5)
-// Copyright (c) 2013-2018 Datenstrom, http://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
+// requires YELLOW 0.8.4 or higher
 
 class YellowOnTime
 {
-	const VERSION = '1.1.5';
-	var $yellow; //access to API
+	const VERSION = '1.2.0';
+	const TYPE = "feature";
+    public $yellow;         //access to API
 	
 	// Handle initialisation
 	function onLoad($yellow)
@@ -16,46 +19,47 @@ class YellowOnTime
 	}
 
 	// Handle page content parsing of custom block
-	function onParseContentBlock($page, $name, $text, $shortcut)
-	{
-		$output = NULL;
-		if($name=="ontime" && $shortcut)
+	public function onParseContentShortcut($page, $name, $text, $type) {
+        $output = null;
+		if($name=="ontime"  && ($type=="block" || $type=="inline"))
 		{
 			list($location, $End, $Start, $mode) = $this->yellow->toolbox->getTextArgs($text);
 			
-			if(empty($location)) {
-					$output = "<h1>No file location → add it!</h1>";
-				    goto TheEnd;
-			}
-			
-			$page = $this->yellow->pages->find($location);
-			if(!$page) {
-				$output .= '<h1><a href="/edit' . $location . '">Create missing file!</a></h1>';
-				goto TheEnd;
-			}	
-			
-			if (empty($End)) { $End = date("Y-m-d"); }
-			if (empty($Start)) { $Start = date("Y-m-d"); }
-					
-			$currdate = date("Y-m-d");
-			
-			if (($currdate >= $Start) AND ($currdate <= $End)) {
-				if(strempty($mode)) $mode = "0";
-				$output .= '<div class="'.$name.'">' . "\n";
-					if($mode == '1') {
-						$output .= '<h2><a href="'.$page->getLocation(true).'">'.$page->getHtml('title')."</a></h2>\n";
-						$output .= $this->yellow->toolbox->createTextDescription($page->getContent(), 0, false, '<!--more-->', ' <a href="'.$page->getLocation(true).'">'.$this->yellow->text->getHtml('blogMore').'</a>');
-					} else {
-						$output .= $page->getContent();
-					}
-				$output .= "</div>\n";
+			if(empty($text)) {
+					$output = '<b>[ontime (/ontime/)file-url end-of-display start_of_display display_mode(1=more)]</b>';
 			}
 			else {
-				$output = "";
+				$location = '/ontime/' . $location;
+				$page = $this->yellow->content->find($location);
+				if(!$page) {
+					$output .= '<h3><u><a href="/edit' . $location . '">'.$this->yellow->text->get("ontime_NoLocation") .'</a></u></h3>';
+				}
+				else {
+					$End = $End?:date("Y-m-d");
+					$Start = $Start?:date("Y-m-d");
+					$currdate = date("Y-m-d");
+				
+					if (($currdate >= $Start) AND ($currdate <= $End)) {
+						if(strempty($mode)) $mode = "0";
+						$output .= '<div class="'.$name.'">' . "\n";
+							if($mode == '1') {
+								$output .= '<h2><a href="'.$page->getLocation(true).'">'.$page->getHtml('title')."</a></h2>\n";
+								$output .= $this->yellow->toolbox->createTextDescription($page->getContent(), 0, false, '<!--more-->', ' <a href="'.$page->getLocation(true).'">'.$this->yellow->text->getHtml('blogMore').'</a>');
+							} else {
+								$output .= $page->getContent();
+							}
+						$output .= "</div>\n";
+					}
+				
+					else {
+						$output = "";
+					}
+				}
 			}
-		}
-		TheEnd:
+		}	
+		
 		return $output;
+	
 	}
 }
 ?>
